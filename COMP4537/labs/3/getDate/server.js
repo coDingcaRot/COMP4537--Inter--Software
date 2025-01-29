1327a987ee6b321f73d563f6dd444dfe57f24b6a
CC <89727825+coDingcaRot@users.noreply.github.com>
2025-01-28 20:31:17 -0800

    Revised Server.js and index.js

--- COMP4537/labs/3/getDate/server.js
+++ COMP4537/labs/3/getDate/server.js
@@ -1,10 +1,9 @@
 const http = require('http');
-
 let date = require('./modules/utils');
 let url = require('url')
 
-const server = http.createServer((req, res) => {
-    let q = url.parse(req.url, true); //Check if the url is live and then print it
+const server = (req, context, callback) => {
+    let q = url.parse(req.path, true); //Check if the url is live and then print it
     
     //get query or ? segment
     let name;
@@ -14,16 +13,20 @@ const server = http.createServer((req, res) => {
         name = "";
     }
 
-    res.writeHead(200, {'Content-Type': 'text/html'}); //header response
-    //Display the message
-    res.write(
-        `<div style="display: flex; justify-content: center; align-items: center; height: 100vh; width: 100vw;">
-          <p style="color: blue; text-align: center;">Hello ${name}, What a beautiful day. Server current date and time is * ${date.getDate()}</p>
-        </div>`
-    )
-    res.end(); //end response 
-}).listen(8888)
+    const html = `
+        <div style="display: flex; justify-content: center; align-items: center; height: 100vh; width: 100vw;">
+            <p style="color: blue; text-align: center;">Hello ${name}, What a beautiful day. Server current date and time is * ${date.getDate()}</p>
+        </div>
+    `;
 
-console.log('Server is running...')
+
+    callback(null, {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html',
+        },
+        body: html,
+    });
+};
 
 module.exports = server
--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,15 +1,13 @@
-// inside netlify/functions/lab3/index.js
 const server = require('../../COMP4537/labs/3/getDate/server');
 
 exports.handler = async (event, context) => {
-
     return new Promise((resolve, reject) => {
         server(event, context, (err, result) => {
-            if(err){
-                reject(err);
-                return
-            }
-            resolve(result);
-        });
-    })
+           if(err){
+             reject(err);
+             return
+         }
+         resolve(result);
+     });
+ })
 };
